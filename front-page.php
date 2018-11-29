be4d16220aa9cc6a91332b0cf21d40814e2348f8
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-11-30 11:09:12 +1300

    Added posts to front page

--- front-page.php
+++ front-page.php
@@ -20,7 +20,23 @@
 					<?php endif; ?>
 
 					<div class="col">
+						<?php if ( have_posts() ): ?>
+							<?php while ( have_posts() ): the_post(); ?>
+								<div class="row p-1 border-bottom border-dark">
+									<?php if ( has_post_thumbnail() ): ?>
+										<div class="col-2">
+											<div class="post-image" style="background-image: url('<?php the_post_thumbnail_url(); ?>')"></div>
+										</div>
+									<?php endif; ?>
 
+									<div class="col">
+										<h2><?php the_title(); ?></h2>
+										<?php the_excerpt(); ?>
+										<a class="btn btn-primary" href="<?php echo esc_url( get_permalink() ); ?>">Read More</a>
+									</div>
+								</div>
+							<?php endwhile; ?>
+						<?php endif; ?>
 					</div>
 
 					<?php if ( is_active_sidebar( 'sidebar-right' ) ): ?>
