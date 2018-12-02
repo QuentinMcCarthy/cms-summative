3447f725b1596407b1d2bbd71a32d70cdfa3db20
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-12-03 11:11:17 +1300

    Added global customisation for link text colour

--- includes/custom_customiser.php
+++ includes/custom_customiser.php
@@ -1,41 +1,64 @@
 <?php
 	/* Table of Contents:
-		1.0:- Header
-			1.1:- Header Background
-			1.2:- Header Text
-			1.3:- Header Logo
-			1.4:- Header Styles
-		2.0:- Carousel
-			2.1:- Carousel Height
-			2.2:- Carousel Styles
-		3.0:- Sidebar
-			3.1:- Sidebar Background
-			3.2:- Sidebar Text
-			3.3:- Sidebar Styles
-		4.0:- Footer
-			4.1:- Footer Background
-			4.2:- Footer Text
-			4.3:- Footer Styles
+		1.0:- Globals
+			1.1:- Link Text
+			1.2:- Globals Styles
+		2.0:- Header
+			2.1:- Header Background
+			2.2:- Header Text
+			2.3:- Header Logo
+			2.4:- Header Styles
+		3.0:- Carousel
+			3.1:- Carousel Height
+			3.2:- Carousel Styles
+		4.0:- Sidebar
+			4.1:- Sidebar Background
+			4.2:- Sidebar Text
+			4.3:- Sidebar Styles
+		5.0:- Footer
+			5.1:- Footer Background
+			5.2:- Footer Text
+			5.3:- Footer Styles
 	*/
 
 
 	add_action( 'customize_register', function ( $wp_customize ) {
 		$img_path = get_template_directory_uri().'/assets/img';
 
-		// 1.0:- Header
+		// 1.0:- Globals
 
-		// 1.1:- Header Background
+		// 1.1:- Link Text
+		$link_text_color_setting_args = array(
+			'default'   => '#007bff',
+			'transport' => 'refresh',
+		);
+
+		$wp_customize->add_setting( 'link_text_color_setting', $link_text_color_setting_args );
+
+		$link_text_color_control_args = array(
+			'label'    => __( 'Link Text Colour', 'cliveschemist' ),
+			'section'  => 'colors',
+			'settings' => 'link_text_color_setting',
+		);
+
+		$link_text_color_control = new WP_Customize_Color_Control( $wp_customize, 'link_text_color_control', $link_text_color_control_args );
+
+		$wp_customize->add_control( $link_text_color_control );
+
+		// 2.0:- Header
+
+		// 2.1:- Header Background
 		$header_bg_color_setting_args = array(
 			'default'   => '#343a40',
 			'transport' => 'refresh',
 		);
 
-		$wp_customize->add_setting( 'header_bg_color_settings', $header_bg_color_setting_args);
+		$wp_customize->add_setting( 'header_bg_color_setting', $header_bg_color_setting_args);
 
 		$header_bg_color_control_args = array(
 			'label'    => __( 'Header Background Colour', 'cliveschemist' ),
 			'section'  => 'colors',
-			'settings' => 'header_bg_color_settings',
+			'settings' => 'header_bg_color_setting',
 		);
 
 		$header_bg_color_control = new WP_Customize_Color_Control( $wp_customize, 'header_bg_color_control', $header_bg_color_control_args );
@@ -43,18 +66,18 @@
 		$wp_customize->add_control( $header_bg_color_control );
 
 
-		// 1.2:- Header Text
+		// 2.2:- Header Text
 		$header_text_color_setting_args = array(
 			'default'   => '#ffffff',
 			'transport' => 'refresh',
 		);
 
-		$wp_customize->add_setting( 'header_text_color_settings', $header_text_color_setting_args );
+		$wp_customize->add_setting( 'header_text_color_setting', $header_text_color_setting_args );
 
 		$header_text_color_control_args = array(
-			'label'   => __( 'Header Text Colour', 'cliveschemist' ),
-			'section' => 'colors',
-			'settings' => 'header_text_color_settings',
+			'label'    => __( 'Header Text Colour', 'cliveschemist' ),
+			'section'  => 'colors',
+			'settings' => 'header_text_color_setting',
 		);
 
 		$header_text_color_control = new WP_Customize_Color_Control( $wp_customize, 'header_text_color_control', $header_text_color_control_args );
@@ -62,18 +85,18 @@
 		$wp_customize->add_control( $header_text_color_control );
 
 
-		// 1.3:- Header Logo
+		// 2.3:- Header Logo
 		$header_logo_setting_args = array(
 			'default'   => $img_path.'/default-header-logo.png',
 			'transport' => 'refresh',
 		);
 
-		$wp_customize->add_setting( 'header_logo_settings', $header_logo_setting_args );
+		$wp_customize->add_setting( 'header_logo_setting', $header_logo_setting_args );
 
 		$header_logo_control_args = array(
 			'label'    => __( 'Header Logo', 'cliveschemist' ),
 			'section'  => 'title_tagline',
-			'settings' => 'header_logo_settings',
+			'settings' => 'header_logo_setting',
 		);
 
 		$header_logo_control = new WP_Customize_Image_Control( $wp_customize, 'header_logo_control', $header_logo_control_args );
@@ -81,7 +104,7 @@
 		$wp_customize->add_control( $header_logo_control );
 
 
-		// 2.0:- Carousel
+		// 3.0:- Carousel
 		$carousel_section_args = array(
 			'title'    => __( 'Carousel', 'cliveschemist' ),
 			'priority' => 20,
@@ -90,13 +113,13 @@
 		$wp_customize->add_section( 'custom_carousel', $carousel_section_args );
 
 
-		// 2.1:- Carousel Height
+		// 3.1:- Carousel Height
 		$carousel_height_setting_args = array(
 			'default'   => '25',
 			'transport' => 'refresh',
 		);
 
-		$wp_customize->add_setting( 'carousel_height_settings', $carousel_height_setting_args );
+		$wp_customize->add_setting( 'carousel_height_setting', $carousel_height_setting_args );
 
 		$carousel_height_input_attrs = array(
 			'min'  => 0,
@@ -107,7 +130,7 @@
 		$carousel_height_control_args = array(
 			'label'       => __( 'Carousel Height', 'cliveschemist' ),
 			'section'     => 'custom_carousel',
-			'settings'    => 'carousel_height_settings',
+			'settings'    => 'carousel_height_setting',
 			'type'        => 'number',
 			'input_attrs' => $carousel_height_input_attrs,
 		);
@@ -117,20 +140,20 @@
 		$wp_customize->add_control( $carousel_height_control );
 
 
-		// 3.0:- Sidebar
+		// 4.0:- Sidebar
 
-		// 3.1:- Sidebar Background
+		// 4.1:- Sidebar Background
 		$sidebar_bg_color_setting_args = array(
 			'default'   => '#afafaf',
 			'transport' => 'refresh',
 		);
 
-		$wp_customize->add_setting( 'sidebar_bg_color_settings', $sidebar_bg_color_setting_args );
+		$wp_customize->add_setting( 'sidebar_bg_color_setting', $sidebar_bg_color_setting_args );
 
 		$sidebar_bg_color_control_args = array(
 			'label'    => __( 'Sidebar Background Colour', 'cliveschemist' ),
 			'section'  => 'colors',
-			'settings' => 'sidebar_bg_color_settings',
+			'settings' => 'sidebar_bg_color_setting',
 		);
 
 		$sidebar_bg_color_control = new WP_Customize_Color_Control( $wp_customize, 'sidebar_bg_color_control', $sidebar_bg_color_control_args );
@@ -138,18 +161,18 @@
 		$wp_customize->add_control( $sidebar_bg_color_control );
 
 
-		// 3.2:- Sidebar Text
+		// 4.2:- Sidebar Text
 		$sidebar_text_color_setting_args = array(
 			'default'   => '#000000',
 			'transport' => 'refresh',
 		);
 
-		$wp_customize->add_setting( 'sidebar_text_color_settings', $sidebar_text_color_setting_args );
+		$wp_customize->add_setting( 'sidebar_text_color_setting', $sidebar_text_color_setting_args );
 
 		$sidebar_text_color_control_args = array(
 			'label'    => __( 'Sidebar Text Colour', 'cliveschemist' ),
 			'section'  => 'colors',
-			'settings' => 'sidebar_text_color_settings',
+			'settings' => 'sidebar_text_color_setting',
 		);
 
 		$sidebar_text_color_control = new WP_Customize_Color_Control( $wp_customize, 'sidebar_text_color_control', $sidebar_text_color_control_args );
@@ -157,20 +180,20 @@
 		$wp_customize->add_control( $sidebar_text_color_control );
 
 
-		// 4.0:- Footer
+		// 5.0:- Footer
 
-		// 4.1:- Footer Background
+		// 5.1:- Footer Background
 		$footer_bg_color_setting_args = array(
 			'default'   => '#ffffff',
 			'transport' => 'refresh',
 		);
 
-		$wp_customize->add_setting( 'footer_bg_color_settings', $footer_bg_color_setting_args );
+		$wp_customize->add_setting( 'footer_bg_color_setting', $footer_bg_color_setting_args );
 
 		$footer_bg_color_control_args = array(
 			'label'    => __( 'Footer Background Colour', 'cliveschemist' ),
 			'section'  => 'colors',
-			'settings' => 'footer_bg_color_settings',
+			'settings' => 'footer_bg_color_setting',
 		);
 
 		$footer_bg_color_control = new WP_Customize_Color_Control( $wp_customize, 'footer_bg_color_control', $footer_bg_color_control_args );
@@ -178,18 +201,18 @@
 		$wp_customize->add_control( $footer_bg_color_control );
 
 
-		// 4.2:- Footer Text
+		// 5.2:- Footer Text
 		$footer_text_color_setting_args = array(
 			'default'   => '#000000',
 			'transport' => 'refresh',
 		);
 
-		$wp_customize->add_setting( 'footer_text_color_settings', $footer_text_color_setting_args );
+		$wp_customize->add_setting( 'footer_text_color_setting', $footer_text_color_setting_args );
 
 		$footer_text_color_control_args = array(
 			'label'    => __( 'Footer Text Colour', 'cliveschemist' ),
 			'section'  => 'colors',
-			'settings' => 'footer_text_color_settings',
+			'settings' => 'footer_text_color_setting',
 		);
 
 		$footer_text_color_control = new WP_Customize_Color_Control( $wp_customize, 'footer_text_color_control', $footer_text_color_control_args );
@@ -202,13 +225,18 @@
 		$img_path = get_template_directory_uri().'/assets/img';
 		?>
 		<style type="text/css">
-			/* 1.4:- Header Styles */
+			/* 1.2:- Globals Styles */
+			a {
+				color: <?php echo get_theme_mod( 'link_text_color_setting', '#007bff' ); ?> !important;
+			}
+
+			/* 2.4:- Header Styles */
 			.header-bg {
-				background-color: <?php echo get_theme_mod( 'header_bg_color_settings', '#343a40' ); ?> !important;
+				background-color: <?php echo get_theme_mod( 'header_bg_color_setting', '#343a40' ); ?> !important;
 			}
 
 			.navbar-dark .navbar-nav .nav-link {
-				color: <?php echo get_theme_mod( 'header_text_color_settings', '#ffffff' ); ?> !important;
+				color: <?php echo get_theme_mod( 'header_text_color_setting', '#ffffff' ); ?> !important;
 				opacity: 0.5;
 			}
 
@@ -216,18 +244,18 @@
 			.navbar-dark .navbar-nav .nav-link.active,
 			.navbar-dark .navbar-nav .nav-link.show,
 			.navbar-dark .navbar-nav .show>.nav-link {
-				color: <?php echo get_theme_mod( 'header_text_color_settings', '#ffffff'); ?> !important;
+				color: <?php echo get_theme_mod( 'header_text_color_setting', '#ffffff'); ?> !important;
 				opacity: 0.9;
 			}
 
 			.navbar-brand-image {
-				background-image: url(<?php echo get_theme_mod( 'header_logo_settings', $img_path.'/default-header-logo.png' ); ?>)
+				background-image: url(<?php echo get_theme_mod( 'header_logo_setting', $img_path.'/default-header-logo.png' ); ?>) !important;
 			}
 
 
-			/* 2.2:- Carousel Styles */
+			/* 3.2:- Carousel Styles */
 			.carousel-image {
-				padding-top: <?php echo get_theme_mod( 'carousel_height_settings', '25' ).'%'; ?> !important;
+				padding-top: <?php echo get_theme_mod( 'carousel_height_setting', '25' ).'%'; ?> !important;
 				width: 100%;
 				background-repeat: no-repeat;
 				background-position: center;
@@ -235,18 +263,18 @@
 			}
 
 
-			/* 3.3:- Sidebar Styles */
+			/* 4.3:- Sidebar Styles */
 			#sidebar-left,
 			#sidebar-right {
-				background-color: <?php echo get_theme_mod( 'sidebar_bg_color_settings', '#afafaf' ); ?> !important;
-				color: <?php echo get_theme_mod( 'sidebar_text_color_settings', '#000000' ); ?> !important;
+				background-color: <?php echo get_theme_mod( 'sidebar_bg_color_setting', '#afafaf' ); ?> !important;
+				color: <?php echo get_theme_mod( 'sidebar_text_color_setting', '#000000' ); ?> !important;
 			}
 
 
-			/* 4.3:- Footer Styles */
+			/* 5.3:- Footer Styles */
 			#main-footer {
-				background-color: <?php echo get_theme_mod( 'footer_bg_color_settings', '#ffffff' ); ?> !important;
-				color: <?php echo get_theme_mod( 'footer_text_color_settings', '#000000' ); ?> !important;
+				background-color: <?php echo get_theme_mod( 'footer_bg_color_setting', '#ffffff' ); ?> !important;
+				color: <?php echo get_theme_mod( 'footer_text_color_setting', '#000000' ); ?> !important;
 			}
 		</style>
 		<?php
