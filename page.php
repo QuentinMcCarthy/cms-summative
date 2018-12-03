b3f12e92b4722af3f62c6ce00458121fc9a26eec
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-12-04 10:49:56 +1300

    Added page file

--- page.php
+++ page.php
@@ -8,19 +8,14 @@
 					<div class="col">
 						<?php if ( have_posts() ): ?>
 							<?php while ( have_posts() ): the_post(); ?>
-								<div class="row p-1">
-									<div id="single-post" class="col">
+								<div class="row p-1 border-bottom border-dark">
+									<div class="col">
 										<?php if ( has_post_thumbnail() ): ?>
 											<div id="single-post-image" class="d-inline-block mr-2 float-left" style="background-image: url('<?php the_post_thumbnail_url(); ?>'); background-color: #<?php echo get_background_color(); ?>;"></div>
 										<?php endif; ?>
 
-										<?php if ( strlen( get_the_title() ) > 0): ?>
-											<h2><?php the_title(); ?></h2>
-										<?php endif; ?>
-
-										<div id="single-post-content" class="pt-3 border-top border-dark">
-											<?php the_content(); ?>
-										</div>
+										<h2><?php the_title(); ?></h2>
+										<?php the_content(); ?>
 									</div>
 								</div>
 							<?php endwhile; ?>
