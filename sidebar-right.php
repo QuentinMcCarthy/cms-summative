3691f47b928a3b417e4224abd98bf1c9c73d5a08
Short Course <short.course@bun2-206-07.local>
2019-02-19 08:54:35 +1300

    Remove unused sidebar files and migrate newer code to template siderbars

--- sidebar-right.php
+++
@@ -1,7 +0,0 @@
-<?php if ( is_active_sidebar( 'sidebar-right' ) ): ?>
-	<div class="col-12 col-md-6 col-lg-5 px-0">
-		<div id="sidebar-right" class="border-bottom border-dark">
-			<?php dynamic_sidebar( 'sidebar-right' ); ?>
-		</div>
-	</div>
-<?php endif; ?>
--- template-sidebar-right.php
+++ template-sidebar-right.php
@@ -3,7 +3,7 @@
 ?>
 
 <?php if ( is_active_sidebar( 'sidebar-right' ) ): ?>
-	<div class="col-4 px-0">
+	<div class="col-12 col-md-6 col-lg-5 px-0">
 		<div id="sidebar-right" class="border-bottom border-dark">
 			<?php dynamic_sidebar( 'sidebar-right' ); ?>
 		</div>
