e50d95a5163a54a958f31248524b64b594255359
Short Course <short.course@bun2-206-07.local>
2019-02-25 11:52:23 +1300

    Minor fixes to code & changed search btn color

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form method="get" id="searchform" class="form-inline search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div role="search">
 		<input id="s" class="form-control mr-sm-2" type="text" name="s" placeholder="Search" value="<?php echo get_search_query(); ?>">
-		<input id="searchsubmit" class="btn btn-primary" type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>">
+		<input id="searchsubmit" class="btn btn-secondary" type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>">
 	</div>
 </form>
